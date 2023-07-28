a20f2dc7d873944d18a9f871e09ea22dc71876b0
Adam Wojciak <Code.AdamW78@gmail.com>
2023-07-27 21:54:08 -0400

    Fixed PuddleBlock and Puddle registry addition

--- src/main/java/eu/midnightdust/puddles/Puddles.java
+++ src/main/java/eu/midnightdust/puddles/Puddles.java
@@ -3,8 +3,6 @@ package eu.midnightdust.puddles;
 import eu.midnightdust.puddles.block.PuddleBlock;
 import eu.midnightdust.puddles.config.PuddlesConfig;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.gamerule.v1.GameRuleFactory;
-import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.fluid.Fluids;
@@ -13,17 +11,17 @@ import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
-import net.minecraft.world.GameRules;
 
 public class Puddles implements ModInitializer {
     public static final String MOD_ID = "puddles";
+    private static final String ITEM_NAME = "puddle";
     public static final Block Puddle = new PuddleBlock(Fluids.WATER, FabricBlockSettings.create());
 
 
     @Override
     public void onInitialize() {
         PuddlesConfig.init(MOD_ID, PuddlesConfig.class);
-        Registry.register(Registries.ITEM, new Identifier(MOD_ID,"puddle"), Puddle);
-        Registry.register(Registries.BLOCK, new Identifier(MOD_ID,"puddle"), new BlockItem(Puddle, new Item.Settings()));
+        Registry.register(Registries.BLOCK, new Identifier(MOD_ID, ITEM_NAME), Puddle);
+        Registry.register(Registries.ITEM, new Identifier(MOD_ID, ITEM_NAME), new BlockItem(Puddle, new Item.Settings()));
     }
 }
