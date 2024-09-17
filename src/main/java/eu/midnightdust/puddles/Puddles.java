b01bcbfb0a77e25c783befb586df118179876d58
Martin Prokoph <motschen@midnightdust.eu>
2024-09-17 14:13:34 +0200

    Backport 2.0.0 to 1.20.1

--- src/main/java/eu/midnightdust/puddles/Puddles.java
+++ src/main/java/eu/midnightdust/puddles/Puddles.java
@@ -5,9 +5,8 @@ import eu.midnightdust.puddles.config.PuddlesConfig;
 import eu.pb4.polymer.core.api.item.PolymerBlockItem;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
-import net.minecraft.entity.attribute.EntityAttributeInstance;
-import net.minecraft.entity.attribute.EntityAttributeModifier;
-import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
 import net.minecraft.network.packet.s2c.play.ParticleS2CPacket;
@@ -21,7 +20,6 @@ import net.minecraft.util.Identifier;
 public class Puddles implements ModInitializer {
     public static final String MOD_ID = "puddles";
     public static final PuddleBlock Puddle = new PuddleBlock();
-    private final static EntityAttributeModifier entityAttributeModifier = new EntityAttributeModifier(id("puddle_speed"), 100, EntityAttributeModifier.Operation.ADD_VALUE);
 
     @Override
     public void onInitialize() {
@@ -29,16 +27,10 @@ public class Puddles implements ModInitializer {
         Registry.register(Registries.BLOCK, id("puddle"), Puddle);
         Registry.register(Registries.ITEM, id("puddle"), new PolymerBlockItem(Puddle, new Item.Settings(), Items.POTION));
         ServerTickEvents.END_WORLD_TICK.register(world -> world.getPlayers().forEach(player -> {
-            EntityAttributeInstance entityAttributeInstance = player.getAttributes().getCustomInstance(EntityAttributes.GENERIC_WATER_MOVEMENT_EFFICIENCY);
             if (player.getBlockStateAtPos().getBlock() instanceof PuddleBlock) {
-                if (entityAttributeInstance != null) {
-                    entityAttributeInstance.removeModifier(entityAttributeModifier);
-                    entityAttributeInstance.addTemporaryModifier(entityAttributeModifier);
-                }
-                if (world.random.nextInt(30) == 0) player.playSoundToPlayer(SoundEvents.ENTITY_PLAYER_SWIM, SoundCategory.BLOCKS, 0.5f, 1.2f);
+                player.addStatusEffect(new StatusEffectInstance(StatusEffects.DOLPHINS_GRACE, 5, 255, true, false, true));
+                if (world.random.nextInt(30) == 0) player.playSound(SoundEvents.ENTITY_PLAYER_SWIM, SoundCategory.BLOCKS, 0.5f, 1.2f);
                 player.networkHandler.sendPacket(new ParticleS2CPacket(ParticleTypes.SPLASH, false, player.getBlockX() + 0.5f, player.getBlockY() + 0.1f, player.getBlockZ() + 0.5f, 0.5f, 0.1f, 0.5f, 1, 2));
-            } else if (player.isInFluid()) {
-                if (entityAttributeInstance != null) entityAttributeInstance.removeModifier(entityAttributeModifier);
             }
         }));
     }
